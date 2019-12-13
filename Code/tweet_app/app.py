ade3617c8011db93ab5e5e25fc7e90968d06953c
FritzHeider <fritz.heider@students.makeschool.com>
2019-12-12 19:04:39 -0800

    adding Procfile

--- Code/tweet_app/app.py
+++ Code/tweet_app/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from sampling import sample, histogram
+from markov import generate_sentence
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+	return render_template('index.html', sentence=generate_sentence())
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
